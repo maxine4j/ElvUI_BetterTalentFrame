5dcaef1209bd9a78aa60a18d193acd261a7ced11
Tim <timothyjings@gmail.com>
2017-04-08 23:21:28 +0800

    Work on talent frame

--- ArwicUIRework.lua
+++ ArwicUIRework.lua
@@ -1,4 +1,5 @@
 local outputPrefix = "ArwicUIRework: "
+local selectedSpec = GetSpecialization()
 
 local function Apply_SpellBook()
     -- vars
@@ -24,9 +25,42 @@ local function Apply_Professions()
     PrimaryProfession1:SetPoint("TOPLEFT", horiMargin, -vertMargin)
 end
 
+local function Apply_Talents()
+    --PlayerTalentFrameSpecializationLearnButton
+
+    local btnActivate = PlayerTalentFrameSpecializationLearnButton
+    btnActivate:SetParent(PlayerTalentFrame)
+    btnActivate:SetScript("OnClick", function(self, button) 
+        SetSpecialization(selectedTalentSpec)
+    end)
+    btnActivate:Enable()
+
+    local tabDim = 35
+    local tabSep = 10
+    local tabYOffset = 35
+    local tabXOffset = 2
+    local numSpecs = GetNumSpecializations()
+    for i = 1, numSpecs, 1 do
+        local specID, specName, specDesc, specIcon, specRole, specPriStat = GetSpecializationInfo(i)
+        local btn = _G["ARWICUIR_btnSpec" .. i]
+        if btn == nil then
+            btn = CreateFrame("Button", "ARWICUIR_btnSpec" .. i, PlayerTalentFrame)
+        end
+        btn:SetPoint("TOPLEFT", PlayerTalentFrame, "TOPRIGHT", tabXOffset, -(tabYOffset + ((tabSep + tabDim) * i)))
+        btn:SetSize(tabDim, tabDim)
+        btn:CreateBackdrop("asasd", true)
+        btn:SetBackdrop("Interface\\Icons\\" .. specIcon)
+        btn:Show()
+        btn:SetScript("OnClick", function(self, button)
+            selectedTalentSpec = i
+        end)
+    end
+end
+
 function ApplyAll()
     Apply_SpellBook()
     Apply_Professions()
+    Apply_Talents()
 end
 
 ApplyAll()
